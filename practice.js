9c5da33873941e134727f6218d7a3c6a595e65c2
Colin Osborn <colin.osborn@gmail.com>
2017-02-26 10:50:04 -0700

    initial commit

---
+++ practice.js
@@ -0,0 +1,8 @@
+function makeTurkeySandwich() {
+
+}
+// much like when ending a sentence with a period, but instead of a period
+// you'll use a semi-colon; Don't forget those semi-colons;
+
+// Then be sure to call the function below or after
+// makeTurkeySandwich()
