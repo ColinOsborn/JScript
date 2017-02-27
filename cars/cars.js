65dd62076bdf427ef6b3e53078f6bae357c92e96
Colin Osborn <colin.osborn@gmail.com>
2017-02-27 13:43:17 -0700

    moving over to object and functions within the object

--- cars/cars.js
+++ cars/cars.js
@@ -1,20 +1,41 @@
-var cars = ["Ferrari", "McLaren", "Red Bull", "Toro Rosso", "Haas"]
+// var cars = ["Ferrari", "McLaren", "Red Bull", "Toro Rosso", "Haas"]
+//
+// function displayCars() {
+//   console.log(cars);
+// }
+//
+// function addCar(car) {
+//   cars.push(car);
+//   displayCars();
+// }
+//
+// function changeCar(position, newCar) {
+//   cars[position] = newCar;
+//   displayCars();
+// }
+//
+// function deleteCar(position) {
+//   cars.splice(position, 1);
+//   displayCars();
+// }
 
-function displayCars() {
-  console.log(cars);
-}
+// This code above here works just fine, but not as robust as we want it.
 
-function addCar(car) {
-  cars.push(car);
-  displayCars();
-}
-
-function changeCar(position, newCar) {
-  cars[position] = newCar;
-  displayCars();
-}
-
-function deleteCar(position) {
-  cars.splice(position, 1);
-  displayCars();
-}
+var f1 = {
+  cars: ["Ferrari", "McLaren", "Haas", "Red Bull", "Toro Rosso"],
+  displayCars: function() {
+    console.log("Formula 1 cars: ", this.cars);
+  },
+  addCar: function(car) {
+    this.cars.push(car);
+    this.displayCars();
+  },
+  changeCar: function(position, newCar) {
+    this.cars[position] = newCar;
+    displayCars();
+  },
+  deleteCar: function(position) {
+    this.cars.splice(position, 1);
+    this.displayCars();
+  }
+};
